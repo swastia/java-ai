63fc16a39fa495bb1f4a9d03fb5c15420fa7a0b8
swastia <swasti.arya.eb@gmail.com>
2026-02-17 07:47:31 +0530

    configuring default chat options

--- src/main/java/com/sarya/springai/config/ChatClientConfig.java
+++ src/main/java/com/sarya/springai/config/ChatClientConfig.java
@@ -4,6 +4,8 @@ package com.sarya.springai.config;
 import com.sarya.springai.advisors.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,7 +16,11 @@ public class ChatClientConfig {
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+       ChatOptions chatOptions =  ChatOptions.builder().model(String.valueOf(OpenAiApi.ChatModel.GPT_4_1_MINI))
+                .temperature(0.8).build();
+
         return chatClientBuilder
+                .defaultOptions(chatOptions)
                 .defaultAdvisors(List.of(new SimpleLoggerAdvisor(), new TokenUsageAuditAdvisor()))
                 .defaultSystem("""
                          You are an internal IT helpdesk assistant. Your role is to assist\s
